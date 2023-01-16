31e95476a3e53fd7c7b484e899c6183835c224ba
Nikita <nikita.derjugin@gmail.com>
2023-01-16 22:49:24 +0200

    lesson135

---
+++ src/components/app/app.js
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
+import './app.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Nikita D.', salary: 800, increase: false, id: 1},
+                {name: 'Erzhan S.', salary: 2000, increase: true, id: 2},
+                {name: 'Arsen L.', salary: 3018, increase: false, id: 3},
+            ]
+        }
+    }
+    
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    render() {
+    return(
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployersList
+                data={this.state.data}
+                onDelete={this.deleteItem} />
+            <EmployersAddForm/>
+        </div>
+    );
+    }
+    
+}
+
+export default App;
